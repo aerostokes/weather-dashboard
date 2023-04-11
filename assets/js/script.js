2c4129b4e4fa2ab2a5a50f7527c1fa9a38fb8340
Rhonda Stokes <rhonda@aerostokes.com>
2023-04-10 21:32:14 -0700

    add 5day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var searchBtn = document.getElementById("search-btn");
 var cityDateEl = document.getElementById("city-date")
 var weatherIconImg = document.getElementById("weather-icon")
 var currentSection = document.getElementById("current")
+var containerSection = document.getElementById("card-container")
+
 
 // on page load: run event listeners
 searchBtn.addEventListener("click", searchGeocode);
@@ -30,6 +32,7 @@ function searchGeocode(event) {
                     console.log(cityStr);
                     cityDateEl.textContent = cityStr + " (" + dayjs().format("M/D/YYYY") + ") ";
                     searchCurrent(lat, lon);
+                    searchForecast(lat, lon);
                 } else { alert("City not found. Try again") };
             });
         } else { alert("An error has occured, try again later.") };
@@ -37,7 +40,7 @@ function searchGeocode(event) {
 };
 
 function searchCurrent(lat, lon) {
-    fetch('https://api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon=' + lon + '&appid=' + apiKey + "&units=imperial").then(function(response) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial").then(function(response) {
         if (response.ok){
             response.json().then(function(data) {
                 console.log(data);
@@ -46,21 +49,65 @@ function searchCurrent(lat, lon) {
                     temperature: "Temp: " + data.main.temp + "°F",
                     wind: "Wind: " + data.wind.speed + "MPH",
                     humidity: "Humidity: " + data.main.humidity + "%"
-                }
+                };
                 writeCurrent (iconURL, currentWeather);
-            })
-        }
+            });
+        } else { alert( "An error has occured, try again later,") };
     })
-}
+};
 
 function writeCurrent (iconURL, currentWeather) {
-    weatherIconImg.setAttribute("src", iconURL) 
+    weatherIconImg.setAttribute("src", iconURL);
     console.log(currentWeather);
     for (key in currentWeather) {
-        var conditionEl = document.createElement("p")
-        conditionEl.textContent = currentWeather[key]
-        currentSection.appendChild(conditionEl)
+        var conditionEl = document.createElement("p");
+        conditionEl.textContent = currentWeather[key];
+        currentSection.appendChild(conditionEl);
     }
 
 }
 
+
+
+function searchForecast(lat, lon) {
+    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial").then(function(response) {
+        if (response.ok){
+            response.json().then(function(data) {
+                console.log(data);
+                for (let i = 2; i <= 34; i+=8) {
+                    var dateData = data.list[i]
+                    dateStr = dayjs.unix(dateData.dt).format("M/D/YYYY");
+                    var iconURL = "https://openweathermap.org/img/wn/" + dateData.weather[0].icon + "@2x.png";
+                    var forecastWeather = {
+                            temperature: "Temp: " + dateData.main.temp + "°F",
+                            wind: "Wind: " + dateData.wind.speed + "MPH",
+                            humidity: "Humidity: " + dateData.main.humidity + "%"
+                        };
+                        writeForecast (dateStr, iconURL, forecastWeather);
+                    }
+            });
+        } else { alert( "An error has occured, try again later,") };
+    });
+};
+
+
+function writeForecast (dateStr, iconURL, forecastWeather) {
+    var newCard = document.createElement("section");
+    newCard.setAttribute("class", "card");
+    containerSection.appendChild(newCard);
+
+    var newTitle = document.createElement("h5");
+    newTitle.textContent = dateStr;
+    newCard.appendChild(newTitle);
+
+    var iconImg = document.createElement("img");
+    iconImg.setAttribute("src", iconURL);
+    newCard.appendChild(iconImg);
+
+    for (key in forecastWeather) {
+        var conditionEl = document.createElement("p");
+        conditionEl.textContent = forecastWeather[key];
+        newCard.appendChild(conditionEl);
+    }
+
+}
